7f26dd0d55b4bb18e10fca5410a7b7f8498a58c2
Thomas Chapeaux <chapeauxthomas@gmail.com>
2012-10-11 14:59:42 +0200

    made the script output to appropriate files (git-ignored) and prepared a graph of the output

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ def compute_best_m(n):
 	return int(math.ceil(n/math.e))
 
 if __name__ == "__main__":
+	fileM = open("output_m.csv", "w")
+	fileV = open("output_value.csv", "w")
 	list_of_m = []
 	for i in range(0, SIZE_OF_SAMPLE, 1):
 		list_of_m.append(i)
@@ -37,6 +39,7 @@ if __name__ == "__main__":
 	current_best_success = 0
 	current_best_m = 0
 	for m in list_of_m:
+		print m, "/", len(list_of_m)
 		successCount = 0
 		testCount = 0
 		for i in range(NUMBER_OF_TESTS):
@@ -50,6 +53,7 @@ if __name__ == "__main__":
 		if (successRate > current_best_success):
 			current_best_success = successRate
 			current_best_m = m
-		print "m:", m, "success:", successRate
+		fileM.write(str(m) + ",\n")
+		fileV.write(str(successRate) + ",\n")
 	print "best m :", current_best_m, "success:", current_best_success
 	print "should be:", compute_best_m(SIZE_OF_SAMPLE)
